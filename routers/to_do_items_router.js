da8e6825cbcef287836faaa2c088a006fcac2a6a
rainwang007 <rainwangjiantao@gmail.com>
2023-07-18 12:52:55 +0800

    createdby route fix

--- routers/to_do_items_router.js
+++ routers/to_do_items_router.js
@@ -4,13 +4,14 @@ const toDoItemController = require('../controllers/to_do_items_controller');
 const authMiddleware = require('../middlewares/auth_middleware');
 
 router.get("/", toDoItemController.getOpenTasks);  //http://localhost:3000/api/toDoItems/
-router.get('/myToDoItems', authMiddleware, toDoItemController.getToDoItems); //http://localhost:3000/api/toDoItems/myToDoItems
+router.get('/createdBy/:userId', toDoItemController.getCreatedByUser); //http://localhost:3000/api/toDoItems/createdBy/:userId
+router.get('/assignedTo/:userId', authMiddleware, toDoItemController.getAssignedToUser); //http://localhost:3000/api/toDoItems/assignedTo/:userId
+// router.get('/myToDoItems', authMiddleware, toDoItemController.getToDoItems); //http://localhost:3000/api/toDoItems/myToDoItems/
 router.get('/:itemId', authMiddleware, toDoItemController.getItemDetail); //http://localhost:3000/api/toDoItems/ +item ID
 router.post('/', authMiddleware, toDoItemController.createToDoItem); //http://localhost:3000/api/toDoItems/
 router.patch('/:itemId', authMiddleware, toDoItemController.updateToDoItem); //http://localhost:3000/api/toDoItems/ + item ID
 router.delete('/:itemId', authMiddleware, toDoItemController.deleteToDoItems); //http://localhost:3000/api/toDoItems/ + itemID
-router.get('/createdBy/:userId', authMiddleware, toDoItemController.getCreatedByUser); //http://localhost:3000/api/toDoItems/createdBy/:userId
-router.get('/assignedTo/:userId', authMiddleware, toDoItemController.getAssignedToUser); //http://localhost:3000/api/toDoItems/assignedTo/:userId
+
 
 
 module.exports = router;
