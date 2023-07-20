142bbb2b2a75fa9b95f9b3b7378eee4f1fa598b8
yekteng <yektengcheh@gmail.com>
2023-07-20 19:41:11 +0800

    idk

--- routers/user_router.js
+++ routers/user_router.js
@@ -7,6 +7,6 @@ router.get('/info', authMiddleware, userController.getUserInfo);//http://localho
 router.post('/register', userController.register);//http://localhost:3000/api/users/register
 router.post('/login', userController.login);//http://localhost:3000/api/users/login
 router.patch('/update', authMiddleware, userController.updateUserDetails);//http://localhost:3000/api/users/update
-router.get('/tasks', authMiddleware, userController.getUserTasks);
+router.get('/tasks', authMiddleware, userController.getUserTasks);//http://localhost:3000/api/users/tasks
 
 module.exports = router;
